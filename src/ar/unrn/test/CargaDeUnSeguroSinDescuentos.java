99ada2456051198768f4bfccac4fe72e3a74cdd9
Licho <l.ojedafernandez@gmail.com>
2021-05-14 17:26:36 -0300

    Se agrego un test para la carga de un seguro que no va a pertenecer a un paquete.

--- src/ar/unrn/test/CargaDeUnSeguroSinDescuentos.java
+++ src/ar/unrn/test/CargaDeUnSeguroSinDescuentos.java
@@ -1,6 +1,5 @@
 package ar.unrn.test;
 
-import ar.unrn.model.SeguroBasico;
 import ar.unrn.model.SeguroMedico;
 import ar.unrn.model.SeguroPaquete;
 import ar.unrn.model.SeguroVida;
@@ -9,10 +8,9 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class CargaSegurosYcalculoDeMonto {
+public class CargaDeUnSeguroSinDescuentos {
 
     private SeguroMedico seguroMedico;
-    private SeguroVida seguroVida;
 
     private SeguroPaquete paquete1;
     private Double montoEsperado;
@@ -21,20 +19,12 @@ public class CargaSegurosYcalculoDeMonto {
     @BeforeEach
     public void Carga() {
         seguroMedico = new SeguroMedico(500.00,"Seguro médico");
-        seguroVida = new SeguroVida(450.00,"seguro vida");
 
-        paquete1 = new SeguroPaquete();
-
-        paquete1.agregarSeguroAlgrupo(seguroMedico);
-        paquete1.agregarSeguroAlgrupo(seguroVida);
-
-       montoEsperado = 902.5;
+        montoEsperado = 500.0;
     }
 
     @Test
     public void calcularMontoSeguro(){
-        assertEquals(montoEsperado,paquete1.obtenerMonto());
+        assertEquals(montoEsperado,seguroMedico.obtenerMonto());
     }
-
-
 }
